6cd251bfd19dd8b536bbdec028ae855e4ba7307d
chris <chris@chris-ThinkPad-T410>
2017-10-02 11:02:29 -0700

    Created "runningSumTrial", made a bunch of progress on DFS for Tickets

--- psychicoptimal/src/TRIAL.java
+++ psychicoptimal/src/TRIAL.java
@@ -26,7 +26,7 @@ public class TRIAL
 	static ArrayList<Ticket> everything = new ArrayList<Ticket>();
 	public static void main (String [] args)
 	{
-		int n = 10, k = 4, j = 3, i = 2;
+		int n = 5, k = 3, j = 3, i = 2;
 		//Set <Integer> something = new TreeSet <Integer>();
 		int[] elements = new int[n];
 		for (int index = 0; index < n; index++)
@@ -41,30 +41,13 @@ public class TRIAL
 		for (int checker:elements)
 			System.out.print(checker + " ");
 		System.out.println();
-		/*
-		for (int l = 0; l < everything.size(); l++)
-		{
-			for (int startingTicket = 0; startingTicket < everything.size(); startingTicket++)
-			{
-				for (Ticket f: everything)
-				{
-					checkCombinationTrial(f, everything.get(startingTicket), l);
-				}
-			}
-		}*/
-		/*for (ArrayList<Integer> a : results)
-		{
-			for (Integer d: a)
-				System.out.print(d + " ");
-			System.out.println();
-		}
-		
-		*/
-		
+
 		// Check entries with brute-force
 		int smallest = everything.size();
+		ArrayList<Integer> smallestSet = new ArrayList<Integer>();
 		for (int indexToCheck = 0; indexToCheck < everything.size(); indexToCheck++)
 		{
+			System.out.println("Starting smallest = " + smallest);
 			ArrayList<Integer> decisions = new ArrayList<Integer>();
 			decisions.add(everything.get(indexToCheck).getID());
 			HashSet<Integer> runningSum = new HashSet<Integer>();
@@ -82,22 +65,46 @@ public class TRIAL
 				}
 			}
 			if (alreadyDone) System.out.println("done early");
-			if (!alreadyDone)
+			if (!alreadyDone || alreadyDone)
 			{
-				ArrayList<Integer> result = runningSumCalculator(everything.get(indexToCheck), decisions, runningSum, elements);
-				if (result != null && result.size() < smallest)
+				for (int ii = indexToCheck+1; ii < everything.size(); ii++)
 				{
-					smallest = result.size();
+					boolean [] passDown = new boolean[everything.size()];
+					passDown[indexToCheck] = true;
+					boolean []  result = runningSumTrial(everything.get(indexToCheck), everything.get(ii), 
+						 passDown, runningSum, elements, smallest);
+
+					int smallestSum = 0;
+					for (int bb = 0; bb < everything.size(); bb++)
+					{
+						if (result[bb] == true) smallestSum++;
+					}
+					if (smallestSum < smallest)
+					{
+						smallestSet.clear();
+						smallest = smallestSum;
+						System.out.print("found a new smallest, at " + smallest + ":\nSmallest-- ");
+						for (int zz = 0; zz < result.length; zz++)
+						{
+							if (result[zz]) 
+							{
+								smallestSet.add(zz);
+								System.out.print(zz + " ");
+							}
+						}
+						System.out.println("root = " + ii);
+						
+					}
 				}
-				
+				//ArrayList<Integer> result = runningSumCalculator(everything.get(indexToCheck), decisions, runningSum, elements, smallest);
 			}
-			System.out.println("Iteration done");
+			//System.out.println("Iteration done");
 		}
-		System.out.println("done, smallest = " + smallest);
+		System.out.println("done, smallest = " + smallest + ", smallest set = " + smallestSet);
 		// CHECKING OUT A BFS FOR SHORTEST, CHECKCOMBINATION IS CURRENTLY A DFS
 		//checkCombinationBFS();
 	}
-	static ArrayList<Integer> runningSumCalculator (Ticket root, ArrayList<Integer> decisions, HashSet<Integer> runningSum, int[] data)
+/*	static ArrayList<Integer> runningSumCalculator (Ticket root, ArrayList<Integer> decisions, HashSet<Integer> runningSum, int[] data, int smallest)
 	{
 		for (Ticket others:everything)
 		{
@@ -124,30 +131,29 @@ public class TRIAL
 						break;
 					}
 				}
-				if (!containsAll)
+				if (!containsAll || containsAll) // CHANGE THIS LATER
 				{
 					for (Ticket a:everything)
 					{
-						ArrayList<Integer> results = runningSumCalculator(a, decisions, runningSum, data);
-						if (results != null)
-							return decisions;
+						ArrayList<Integer> results = runningSumCalculator(others, decisions, runningSum, data, smallest);
+						if (results.size() < smallest) 
+						{
+							smallest = results.size();
+
+						}
 					}
 				}
-				else 
-				{ 
-					return decisions;
-				}
 			}
-			//System.out.println("one repeated or root: " + others.getID());
+			//System.out.println("one repeated or root: " + others.getID())
 		}
-		return null;
+		return decisions; 
 
-	}
+	}*/
 	static void generateRecursive(int arr[], int data[], int start, int end, int index, int r)
 	{
 		if (index == r)
 		{
-			ArrayList<Integer> entry = new ArrayList<Integer> ();
+			//ArrayList<Integer> entry = new ArrayList<Integer> ();
 			Ticket toAdd = new Ticket();
 			for (int j=0; j<r; j++)
 			{
@@ -165,7 +171,7 @@ public class TRIAL
 	}
 
 	//static boolean checkCombinationHelper (int factor, int index, int[] data)
-	{
+	//{
 		/*
 		for (int sumOf = 0; sumOf <= factor; sumOf++)
 		{
@@ -182,7 +188,7 @@ public class TRIAL
 			}
 		}
 		return false;
-		*/
+		
 		
 	}
 	static HashSet<Integer> checkCombination (int factor, int startIndex, int add, int[] data)
@@ -197,6 +203,60 @@ public class TRIAL
 			return toReturn;
 		}
 	}
+	*/
+static boolean[]  runningSumTrial (Ticket root, Ticket target, boolean[] decisions, HashSet<Integer> runningSum, int[] data, int smallest)
+{
+	if (target.getID() != root.getID()&& decisions[target.getID()] == false)
+	{
+		decisions[target.getID()] = true;
+		runningSum.addAll(target.getNumbers());
+		System.out.print("Checking: ");
+		for (int i = 0; i < decisions.length; i++)
+		{
+			if (decisions[i])
+				System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		boolean containsAll = true;
+		for (int checkNext: data)
+		{
+		
+			if (!runningSum.contains(checkNext))
+			{
+				containsAll = false;
+				break;
+			}
+		}
+		if (!containsAll) // CHANGE THIS LATER
+		{
+			for (Ticket a:everything)
+			{
+				if (a.getID() > target.getID())
+				{
+					//System.out.println("going to call on ROOT = " + root.getID() +  ", target = " + a.getID());
+					boolean[] newResults = runningSumTrial(target, a, decisions, runningSum, data, smallest);
+					int smallestSum = newResults.length;
+					for (int bb = 0; bb < decisions.length; bb++)
+					{
+						if (newResults[bb] == true) smallestSum++;
+					}
+					if (smallestSum < smallest) 
+					{
+						smallest = smallestSum;
+						System.out.println("found a new smallest INSIDE, at " + smallest + ": ");
+						for (int zz = 0; zz < newResults.length; zz++)
+							if (newResults[zz]) System.out.print(zz + " ");
+						System.out.println();
+					}
+				}
+			}
+		}
+	}
+	//System.out.println("root and target not working: " + root.getID() + ", " + target.getID());
+	return decisions; 
+
+}
 }
 /*	static ArrayList<Ticket> checkCombinationBFS (int [] data, int layersDown)
 	{
