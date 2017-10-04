ac7c9135e105c0bfbd1e1e35f0773fe92f8935f7
chris <chris@chris-ThinkPad-T410>
2017-10-03 22:14:46 -0700

    COMPLETED BRUTE FORCE!! WORKS FOR 5 3 3 2, PLEASE TEST

--- psychicoptimal/src/TRIAL.java
+++ psychicoptimal/src/TRIAL.java
@@ -57,21 +57,23 @@ class Combination
 	}
 }
 
-
 public class TRIAL 
 {
+	static int smallest = Integer.MAX_VALUE;
+	static LinkedList<Ticket> bestPath = null;
 	static ArrayList<Ticket> everything = new ArrayList<Ticket>();
 	static ArrayList<Combination> possibilities = new ArrayList<Combination>(); 
 	public static void main (String [] args)
 	{
-		int n = 4, k = 3, j = 2, l = 2;
-		//Set <Integer> something = new TreeSet <Integer>();
+		int n = 5, k = 3, j = 3, l = 2;
 		
 		// Setup ticket range
 		int[] elements = new int[n];
 		for (int index = 0; index < n; index++)
 			elements[index] = index + 1; 
 		int [] data = new int [n];
+		
+		// Generate Possible Tickets of size k
 		generateRecursive(elements, data, 0, n-1, 0, k, true);
 		for (Ticket e:everything)
 		{
@@ -81,11 +83,10 @@ public class TRIAL
 			System.out.print(checker + " ");
 		System.out.println();
 		
-		
-		// Setup pro mised number range
+		// Setup promised number range
 		int[] givenData = new int[n];
 
-		// Generate combinations of promised numbers
+		// Generate combinations of promised numbers j
 		generateRecursive(elements, givenData, 0, n-1, 0, j, false);
 	
 			
@@ -97,91 +98,30 @@ public class TRIAL
 		System.out.println(possibilities.size());
 		
 		
-		runningSumBruteForce(everything.get(0), null, elements, l);
-		/*
-		for (int indexC = 0; indexC < possibilities.size(); indexC++)
+		
+		// DFS Search all combinations for possible smallest values
+		for (int startingIndex = 0; startingIndex < everything.size(); startingIndex++)
 		{
-			Ticket e = possibilities.get(indexC);
-			System.out.println(e.getID() + ":  " + e.getNumbers() + ", " + possibilityScan.get(indexC).toString());
+			runningSumBruteForce(everything.get(0), null, elements, l);
+			// DONT UNDERESTIMATE THIS, RUNS in 2^n
 		}
 		
-		*/
-		
-		
-		
-		/*
-		// Check entries with brute-force
-		int smallest = everything.size();
-		ArrayList<Integer> smallestSet = new ArrayList<Integer>();
-		for (int indexToCheck = 0; indexToCheck < everything.size(); indexToCheck++)
+		System.out.println("RESULTS-----------------------");
+		System.out.println("Smallest = " + smallest);
+		if (bestPath != null)
 		{
-			System.out.println("Starting smallest = " + smallest);
-			ArrayList<Integer> decisions = new ArrayList<Integer>();
-			decisions.add(everything.get(indexToCheck).getID());
-			HashSet<Integer> runningSum = new HashSet<Integer>();
-			runningSum.addAll(everything.get(indexToCheck).getNumbers());
-			
-			System.out.println("Checking: " + decisions.get(0));
-			boolean alreadyDone = true;
-			for (int checkNext: elements)
-			{
-				if (!runningSum.contains(checkNext))
-				{
-					// Not quite
-					alreadyDone = false;
-					break;
-				}
-			}
-			if (alreadyDone) System.out.println("done early");
-			if (!alreadyDone || alreadyDone)
-			{
-				for (int ii = indexToCheck+1; ii < everything.size(); ii++)
-				{
-					boolean [] passDown = new boolean[everything.size()];
-					passDown[indexToCheck] = true;
-					boolean []  result = runningSumTrial(everything.get(indexToCheck), everything.get(ii), 
-						 passDown, runningSum, elements, smallest);
-
-					int smallestSum = 0;
-					for (int bb = 0; bb < everything.size(); bb++)
-					{
-						if (result[bb] == true) smallestSum++;
-					}
-					if (smallestSum < smallest)
-					{
-						smallestSet.clear();
-						smallest = smallestSum;
-						System.out.print("found a new smallest, at " + smallest + ":\nSmallest-- ");
-						for (int zz = 0; zz < result.length; zz++)
-						{
-							if (result[zz]) 
-							{
-								smallestSet.add(zz);
-								System.out.print(zz + " ");
-							}
-						}
-						System.out.println("root = " + ii);
-						
-					}
-				}
-				//ArrayList<Integer> result = runningSumCalculator(everything.get(indexToCheck), decisions, runningSum, elements, smallest);
-			}
-			//System.out.println("Iteration done");
+			for (Ticket winner:bestPath) 
+				System.out.println(winner.getNumbers());
 		}
-		System.out.println("done, smallest = " + smallest + ", smallest set = " + smallestSet);
-		// CHECKING OUT A BFS FOR SHORTEST, CHECKCOMBINATION )IS CURRENTLY A DFS
-		//checkCombinationBFS();
-		 */
-		 
 	}
 	
 	static LinkedList<Ticket> runningSumBruteForce (Ticket root, LinkedList<Ticket> decisions, int[] data, int requiredToContain)
 	{
 		if (decisions == null) decisions = new LinkedList<Ticket>();
 		
-		System.out.print("Checking " + root.getNumbers() + " with ");
+		System.out.print("Checking " + root.getID() + " with ");
 		for (Ticket che:decisions)
-			System.out.print(che.getNumbers() + " " );
+			System.out.print(che.getID()+ " " );
 		System.out.println();
 		// Check the combinations on this list of tickets
 		boolean allFound = true;
@@ -200,7 +140,6 @@ public class TRIAL
 				}
 				if (!foundElsewhere) allFound = false;
 			}
-			//System.out.println("Root contains " + each.getNumbers());
 		}
 		
 		
@@ -210,25 +149,25 @@ public class TRIAL
 			System.out.println("Not all were found");
 			for (int index = root.getID() + 1; index < everything.size(); index++)
 			{
-				LinkedList<Ticket> newDecisions = decisions;
+				LinkedList<Ticket> newDecisions = new LinkedList<Ticket>(decisions);
 				newDecisions.add(root);
 				LinkedList<Ticket> found = runningSumBruteForce(everything.get(index), newDecisions, data, requiredToContain);
 				if (found != null) 
 				{
-					/*System.out.println("Found a winner:" );
-					for (Ticket winner:found) 
-						System.out.println(winner.getNumbers());
-					System.out.println("END WINNER");
-					*/
+					if (found.size() < smallest)
+					{
+						bestPath = new LinkedList<Ticket>(found);
+						smallest = found.size();
+					}
 				}
 			}
 			return null;
 		}
 		else 
 		{
-			// All combinations are covered, return the decisions list
+			// All combinations are covered, return the decisions list and root
+			decisions.add(root);
 			System.out.println("Found a working one");
-			System.out.println(root.getNumbers());
 			for (Ticket d:decisions) 
 				System.out.println(d.getNumbers());
 			return decisions;
@@ -243,7 +182,6 @@ public class TRIAL
 	{
 		if (index == r)
 		{
-			//ArrayList<Integer> entry = new ArrayList<Integer> ();
 			if (ticket) 
 			{
 				Ticket toAdd = new Ticket();
@@ -271,58 +209,4 @@ public class TRIAL
 			generateRecursive(arr, data, i+1, end, index+1, r, ticket);
 		}
 	}
-	
-static boolean[]  runningSumTrial (Ticket root, Ticket target, boolean[] decisions, HashSet<Integer> runningSum, int[] data, int smallest)
-{
-	if (target.getID() != root.getID()&& decisions[target.getID()] == false)
-	{
-		decisions[target.getID()] = true;
-		runningSum.addAll(target.getNumbers());
-		System.out.print("Checking: ");
-		for (int i = 0; i < decisions.length; i++)
-		{
-			if (decisions[i])
-				System.out.print(i + " ");
-		}
-		System.out.println();
-		
-		boolean containsAll = true;
-		for (int checkNext: data)
-		{
-		
-			if (!runningSum.contains(checkNext))
-			{
-				containsAll = false;
-				break;
-			}
-		}
-		if (!containsAll) // CHANGE THIS LATER
-		{
-			for (Ticket a:everything)
-			{
-				if (a.getID() > target.getID())
-				{
-					//System.out.println("going to call on ROOT = " + root.getID() +  ", target = " + a.getID());
-					boolean[] newResults = runningSumTrial(target, a, decisions, runningSum, data, smallest);
-					int smallestSum = newResults.length;
-					for (int bb = 0; bb < decisions.length; bb++)
-					{
-						if (newResults[bb] == true) smallestSum++;
-					}
-					if (smallestSum < smallest) 
-					{
-						smallest = smallestSum;
-						System.out.println("found a new smallest INSIDE, at " + smallest + ": ");
-						for (int zz = 0; zz < newResults.length; zz++)
-							if (newResults[zz]) System.out.print(zz + " ");
-						System.out.println();
-					}
-				}
-			}
-		}
-	}
-	//System.out.println("root and target not working: " + root.getID() + ", " + target.getID());
-	return decisions; 
-
-}
 }
